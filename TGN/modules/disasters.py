6cb83d2467eb50b2d21782ca4a92611db32f3f98
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-17 16:00:23 +0530

    Update disasters.py

--- TGN/modules/disasters.py
+++ TGN/modules/disasters.py
@@ -24,7 +24,7 @@ from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "Emli/elevated_users.json")
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "TGN/elevated_users.json")
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
